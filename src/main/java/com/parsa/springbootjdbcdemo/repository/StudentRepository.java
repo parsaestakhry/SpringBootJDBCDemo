2b797b8bdefe22014fed30baf3af47d7b1c30295
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-24 21:26:38 +0330

    add StudentRepository for data persistence and integrate with StudentService

---
+++ src/main/java/com/parsa/springbootjdbcdemo/repository/StudentRepository.java
@@ -0,0 +1,13 @@
+package com.parsa.springbootjdbcdemo.repository;
+
+
+import com.parsa.springbootjdbcdemo.model.Student;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentRepository {
+
+    public void save(Student student) {
+
+    }
+}
--- src/main/java/com/parsa/springbootjdbcdemo/service/StudentService.java
+++ src/main/java/com/parsa/springbootjdbcdemo/service/StudentService.java
@@ -1,12 +1,26 @@
 package com.parsa.springbootjdbcdemo.service;
 
 import com.parsa.springbootjdbcdemo.model.Student;
+import com.parsa.springbootjdbcdemo.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
 
+
+    private StudentRepository studentRepository;
+
+    public StudentRepository getStudentRepository() {
+        return studentRepository;
+    }
+
+    @Autowired
+    public void setStudentRepository(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public void addStudent(Student student) {
-        System.out.println("adding student");
+        studentRepository.save(student);
     }
 }

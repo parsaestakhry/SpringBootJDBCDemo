c3fc731e7c315c42721f38c3f632bfb63d57eccb
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-25 09:58:58 +0330

    add schema and data scripts for student table creation and initial records

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO student(rollNo, name, marks) VALUES (401102, "Parsa Estakhri", 72);
+INSERT INTO student(rollNo, name, marks) VALUES (401103, "Shayan A-Pouya", 73);
+
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS student (
+    rollNo INT PRIMARY KEY,
+    name varchar(50),
+    marks INT
+
+)

6e14c5335e229dba64f00d0166db17f3023cdeb4
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-25 09:59:10 +0330

    add JdbcTemplate integration to StudentRepository for database operations

--- src/main/java/com/parsa/springbootjdbcdemo/repository/StudentRepository.java
+++ src/main/java/com/parsa/springbootjdbcdemo/repository/StudentRepository.java
@@ -2,6 +2,8 @@ package com.parsa.springbootjdbcdemo.repository;
 
 
 import com.parsa.springbootjdbcdemo.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -10,8 +12,21 @@ import java.util.List;
 @Repository
 public class StudentRepository {
 
-    public void save(Student student) {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void save(Student student) {
+        String sql = "INSERT INTO student(rollNo,name,marks) VALUES(?,?,?)";
+        int rows = jdbcTemplate.update(sql, student.getRollNo(), student.getName(), student.getMarks());
+        System.out.println("rows affected:" + rows);
     }
 
     public List<Student> findAll() {
